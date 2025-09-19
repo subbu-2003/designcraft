ef4b164ca61389362e06d1c41a973a4fd220854a
SubburamK <subburam@e2o.in>
2025-09-20 02:41:09 +0530

    add service and portfolio and teams

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ServicesComponent } from './services/services.component';
+import { OurteamComponent } from './ourteam/ourteam.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 export const routes: Routes = [
 
@@ -10,5 +12,11 @@ export const routes: Routes = [
     },
     {
         path:'service', component:ServicesComponent
+    },
+    {
+        path:'team', component: OurteamComponent
+    },
+    {
+        path:'portfolio', component: PortfolioComponent
     }
 ];
---
+++ src/app/ourteam/ourteam.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ourteam',
+  standalone: true,
+  imports: [],
+  templateUrl: './ourteam.component.html',
+  styleUrl: './ourteam.component.scss'
+})
+export class OurteamComponent {
+
+}
